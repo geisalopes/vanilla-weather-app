6b687db71af83cb3853603ea63af81608681fac5
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-08-29 14:17:22 +0200

    update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+// Definir temperatura atual, bem como cidade, vento e umidade
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -9,11 +11,42 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = response.data.wind.speed;
-  console.log(response.data.wind.speed);
+  console.log(response.data);
 }
 
 let apiKey = "bf12f0ob06f7acf048dt44a41aadd939";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Berlin&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Sapucaia do Sul&key=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
+
+// Definir dia e hora atual
+
+function showCurrentDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#date");
+let currentDate = new Date();
+dateElement.innerHTML = showCurrentDate(currentDate);
